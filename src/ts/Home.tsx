cf72b2df4ee108916c3c336c931055400b0da6dd
Nick Senger <dev@nsenger.com>
2017-03-18 23:19:47 -0700

    feat: Removes unnecessary div wrapper

--- src/ts/Home.tsx
+++ src/ts/Home.tsx
@@ -22,54 +22,52 @@ export default class HomeComponent extends React.Component<null, ComponentState>
 
     render() {
         return (
-            <div>
-                <Scene style={{ width: '100vw', height: '100vh' }} >
-                    <Locate position="top-right" />
-                    <Graphic>
-                        <Polygon geometryProperties={{
-                            rings: [
-                                [-64.78, 32.3],
-                                [-66.07, 18.45],
-                                [-80.21, 25.78],
-                                [-64.78, 32.3]
-                            ]
-                        }} />
-                        <SimpleFillSymbol symbolProperties={{
-                            color: [227, 139, 79, 0.8],
-                            outline: {
-                                color: [255, 255, 255],
-                                width: 1
-                            }
-                        }} />
-                    </Graphic>
-                    <Graphic>
-                        <Polyline geometryProperties={{
-                            paths: [
-                                [-111.30, 52.68],
-                                [-98, 49.5],
-                                [-93.94, 29.89]
-                            ]
-                        }} />
-                        <SimpleLineSymbol symbolProperties={{
-                            color: [226, 119, 40],
-                            width: 4
-                        }} />
-                    </Graphic>
-                    <Graphic>
-                        <Point geometryProperties={{
-                            longitude: -49.97,
-                            latitude: 41.73
-                        }} />
-                        <SimpleMarkerSymbol symbolProperties={{
-                            color: [226, 119, 40],
-                            outline: {
-                                color: [255, 255, 255],
-                                width: 2
-                            }
-                        }} />
-                    </Graphic>
-                </Scene>
-            </div>
+            <Scene style={{ width: '100vw', height: '100vh' }} >
+                <Locate position="top-right" />
+                <Graphic>
+                    <Polygon geometryProperties={{
+                        rings: [
+                            [-64.78, 32.3],
+                            [-66.07, 18.45],
+                            [-80.21, 25.78],
+                            [-64.78, 32.3]
+                        ]
+                    }} />
+                    <SimpleFillSymbol symbolProperties={{
+                        color: [227, 139, 79, 0.8],
+                        outline: {
+                            color: [255, 255, 255],
+                            width: 1
+                        }
+                    }} />
+                </Graphic>
+                <Graphic>
+                    <Polyline geometryProperties={{
+                        paths: [
+                            [-111.30, 52.68],
+                            [-98, 49.5],
+                            [-93.94, 29.89]
+                        ]
+                    }} />
+                    <SimpleLineSymbol symbolProperties={{
+                        color: [226, 119, 40],
+                        width: 4
+                    }} />
+                </Graphic>
+                <Graphic>
+                    <Point geometryProperties={{
+                        longitude: -49.97,
+                        latitude: 41.73
+                    }} />
+                    <SimpleMarkerSymbol symbolProperties={{
+                        color: [226, 119, 40],
+                        outline: {
+                            color: [255, 255, 255],
+                            width: 2
+                        }
+                    }} />
+                </Graphic>
+            </Scene>
         )
     }
 }
