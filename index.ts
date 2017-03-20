4fdcedb25703a21b0035f88345a9c3d066fbbe15
Nick Senger <nick9079@esri.com>
2017-03-20 14:58:23 -0700

    feat: Preps for initial publication

---
+++ index.ts
@@ -0,0 +1,16 @@
+import { Map, Scene } from './src/ts/components/ArcComposites';
+import * as Geometry from './src/ts/components/geometry/GeometryComposites';
+import Graphic from './src/ts/components/graphics/Graphic';
+import * as Layers from './src/ts/components/layers/LayerComposites';
+import * as Symbols from './src/ts/components/symbols/SymbolComposites';
+import * as Widgets from './src/ts/components/widgets/WidgetComposites';
+
+export default {
+    Graphic,
+    Map,
+    Scene,
+    Geometry,
+    Layers,
+    Symbols,
+    Widgets
+};
