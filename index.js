8a3c12f126fd5f5f93e3d3f4dcbcba847bf45481
Nick Senger <nick9079@esri.com>
2017-03-20 15:26:38 -0700

    chore: Updates index.js for initial publication

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ArcComposites_1 = require("./src/ts/components/ArcComposites");
+var Geometry = require("./src/ts/components/geometry/GeometryComposites");
+var Graphic_1 = require("./src/ts/components/graphics/Graphic");
+var Layers = require("./src/ts/components/layers/LayerComposites");
+var Symbols = require("./src/ts/components/symbols/SymbolComposites");
+var Widgets = require("./src/ts/components/widgets/WidgetComposites");
+exports.default = {
+    Graphic: Graphic_1.default,
+    Map: ArcComposites_1.Map,
+    Scene: ArcComposites_1.Scene,
+    Geometry: Geometry,
+    Layers: Layers,
+    Symbols: Symbols,
+    Widgets: Widgets
+};
 //# sourceMappingURL=index.js.map
