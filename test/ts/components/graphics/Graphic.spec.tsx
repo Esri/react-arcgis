2e5e12c9b45aab212d2680cb551ca2a029af305d
Nick Senger <dev@nsenger.com>
2017-06-05 13:40:15 -0700

    chore: Updates testing for Graphic

--- test/ts/components/graphics/Graphic.spec.tsx
+++ test/ts/components/graphics/Graphic.spec.tsx
@@ -1,18 +1,62 @@
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import * as React from 'react';
 import * as sinon from 'sinon';
 import Graphic from '../../../../src/ts/components/graphics/Graphic';
 
 export default () => (
     describe('Graphic', () => {
-        let graphic;
-        beforeEach(() => {
-            graphic = shallow(<Graphic />);
+        describe('as a shallow component', () => {
+            let graphic;
+            beforeEach(() => {
+                graphic = shallow(<Graphic />);
+            });
+
+            it('should exist', () => {
+                expect(graphic).to.exist;
+            });
         });
 
-        it('should exist', () => {
-            expect(graphic).to.exist;
+        describe('as a mounted component', () => {
+            let graphic
+            beforeEach(() => {
+                sinon.spy(Graphic.prototype, 'componentDidMount');
+                graphic = mount(<Graphic />);
+            });
+
+            it('should call componentDidMount', () => {
+                expect(Graphic.prototype.componentDidMount['callCount']).to.equal(1);
+            });
+
+            describe('esriPromise succeeds', () => {
+                before(() => {
+                    global['asyncSuccess'] = true;
+                });
+
+                beforeEach(() => {
+                    graphic = mount(<Graphic />);
+                });
+
+                it('should call renderGraphic', (done) => {
+                    graphic.instance().renderGraphic = sinon.stub();
+                    setTimeout(() => {
+                        expect(graphic.instance().renderGraphic.callCount).to.equal(1);
+                        done();
+                    }, 1);
+                });
+
+                after(() => {
+                    global['asyncSuccess'] = false;
+                });
+            });
+
+            describe('esriPromise fails', () => {
+
+            });
+
+            afterEach(() => {
+                Graphic.prototype.componentDidMount['restore']();
+            });
         });
     })
 );
