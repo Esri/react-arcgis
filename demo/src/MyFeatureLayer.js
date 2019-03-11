649fd49ca9e16ef502c18b6b82a52998139fb7a1
Abdelrahman <abdel.abdelrazek@gmail.com>
2019-03-11 07:44:48 -0400

    use hooks to replace componentWillMount and ComponentWillUnmount

--- demo/src/MyFeatureLayer.js
+++ demo/src/MyFeatureLayer.js
@@ -1,30 +1,26 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { loadModules } from 'react-arcgis';
 
-export default class MyFeatureLayer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            myFeatureLayer: null
-        };
-    }
+const MyFeatureLayer = (props) => {
 
-    render() {
-        return null;
-    }
+    const [myFeatureLayer, setMyFeatureLayer] = useState(null);
+    useEffect(() => {
 
-    componentWillMount() {
-        loadModules(['esri/layers/FeatureLayer']).then(([ FeatureLayer ]) => {
+        loadModules(['esri/layers/FeatureLayer']).then(([FeatureLayer]) => {
             const myFeatureLayer = new FeatureLayer({
-                url: this.props.featureLayerProperties.url
-              });
+                url: props.featureLayerProperties.url
+            });
 
-            this.setState({ myFeatureLayer });
-            this.props.map.add(myFeatureLayer);
+            setMyFeatureLayer(myFeatureLayer);
+            props.map.add(myFeatureLayer);
         }).catch((err) => console.error(err));
-    }
 
-    componentWillUnmount() {
-        this.props.map.remove(this.state.myFeatureLayer);
-    }
+        return function cleanup() {
+            props.map.remove(myFeatureLayer);
+        }
+    }, []);
+
+    return null;
 }
+
+export default MyFeatureLayer;
